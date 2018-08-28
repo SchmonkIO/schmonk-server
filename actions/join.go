596831193add1748a1d9dd863a6cc1bb6157b34c
Yannik <yannik@idle.lol>
2018-08-28 13:46:04 +0200

    Fixed incorrect serialization in join action

--- actions/join.go
+++ actions/join.go
@@ -1,6 +1,8 @@
 package actions
 
 import (
+	"encoding/json"
+	
 	"github.com/schmonk.io/schmonk-server/global"
 	"github.com/schmonk.io/schmonk-server/models"
 	"github.com/schmonk.io/schmonk-server/util"
@@ -12,31 +14,8 @@ type JoinAction struct {
 }
 
 func Join(player *models.BasePlayer, message []byte, mt int) {
-	// data := models.NameRequest{}
-	// err := json.Unmarshal(message, &data)
-	// if err != nil {
-	// 	util.LogToConsole(err.Error())
-	// 	player.Connection.WriteMessage(mt, []byte("invalid json"))
-	// } else {
-	// 	player.Name = data.Name
-	// 	player.Color = util.GetRandomColor()
-	// 	player.Status = "lobby"
-	// 	global.Mutex.Lock()
-	// 	global.Players[player.ID.Hex()] = player
-	// 	global.Mutex.Unlock()
-	// 	util.LogToConsole(player.ID.Hex())
-	// 	global.Mutex.Lock()
-	// 	util.LogToConsole(global.Players[player.ID.Hex()])
-	// 	global.Mutex.Unlock()
-	// 	SendPlayerList(mt)
-	// 	global.Mutex.Lock()
-	// 	l := len(global.Players)
-	// 	global.Mutex.Unlock()
-	// 	util.LogToConsole("Connected Players:", l)
-	// }
-
 	data := JoinAction{}
-	err := data.Unmarshal(message)
+	err := json.Unmarshal(message, &data)
 	if err != nil {
 		util.LogToConsole(err.Error())
 		player.Connection.WriteMessage(mt, []byte("invalid json"))
